e17d5ac03f6cc012d54ce522048877442a1d971b
PatilDivyani <125538181+PatilDivyani@users.noreply.github.com>
2023-05-13 19:20:05 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,26 +1,27 @@
-//your code here
-const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'The Rolling Stones', 'Anthrax'];
+const bandNames = ["The Beatles", "Led Zeppelin", "The Rolling Stones", "Pink Floyd", "Aerosmith", "Queen", "Anthrax"];
 
-// Define articles to exclude
-const articles = ['a', 'an', 'the'];
-
-// Sort band names in lexicographic order while excluding articles
-const sortedBandNames = bandNames.sort((a, b) => {
-  a = a.replace(/^(a |an |the )/i, '').trim();
-  b = b.replace(/^(a |an |the )/i, '').trim();
-  if (a < b) {
-    return -1;
+// Define a function to remove articles from the beginning of band names
+function removeArticles(name) {
+  const articles = ["a ", "an ", "the "];
+  for (let article of articles) {
+    if (name.toLowerCase().startsWith(article)) {
+      return name.slice(article.length);
+    }
   }
-  if (a > b) {
-    return 1;
-  }
-  return 0;
-});
+  return name;
+}
 
-// Display sorted band names as list items inside ul#band
-const bandList = document.querySelector('#band');
-sortedBandNames.forEach((bandName) => {
-  const listItem = document.createElement('li');
-  listItem.textContent = bandName;
-  bandList.appendChild(listItem);
-});
+// Sort the band names in lexicographic order (excluding articles)
+const sortedBandNames = bandNames.sort((a, b) => removeArticles(a).localeCompare(removeArticles(b)));
+
+// Create an unordered list in HTML format and insert the sorted band names as list items
+const ul = document.createElement("ul");
+ul.setAttribute("id", "band");
+for (let name of sortedBandNames) {
+  const li = document.createElement("li");
+  li.textContent = name;
+  ul.appendChild(li);
+}
+
+// Append the unordered list to the document body
+document.body.appendChild(ul);
